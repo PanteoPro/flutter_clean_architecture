b3f7144e03570384229908487a3f41d3773b76e1
PanteoPro <panteopro@gmail.com>
2022-01-29 14:28:23 +0300

    injection dependency of bloc

--- lib/internal/application.dart
+++ lib/internal/application.dart
@@ -1,5 +1,7 @@
+import 'package:clean_arhitecture_example_app/internal/dependencies/home_module.dart';
 import 'package:clean_arhitecture_example_app/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
@@ -12,7 +14,10 @@ class Application extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomeScreen(),
+      home: BlocProvider(
+        create: (context) => HomeModule.homeBloc(),
+        child: const HomeScreen(),
+      ),
     );
   }
 }
