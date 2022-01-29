07798fe51ed3b68e1faf895725bc8b5b5fe29f65
PanteoPro <panteopro@gmail.com>
2022-01-29 14:26:36 +0300

    fix mapper

--- lib/data/mapper/day_mapper.dart
+++ lib/data/mapper/day_mapper.dart
@@ -4,8 +4,8 @@ import 'package:clean_arhitecture_example_app/domain/model/day.dart';
 class DayMapper {
   static Day fromApi(ApiDay day) => Day(
         sunrise: DateTime.tryParse(day.sunrise) ?? DateTime(0),
-        sunset: DateTime.tryParse(day.sunrise) ?? DateTime(0),
-        solarNoon: DateTime.tryParse(day.sunrise) ?? DateTime(0),
+        sunset: DateTime.tryParse(day.sunset) ?? DateTime(0),
+        solarNoon: DateTime.tryParse(day.solarNoon) ?? DateTime(0),
         dayLength: day.dayLength.toInt(),
       );
 }
