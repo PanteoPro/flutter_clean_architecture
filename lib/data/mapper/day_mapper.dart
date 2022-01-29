e13fb5fda85c5c0954d856a01647655068fae491
PanteoPro <panteopro@gmail.com>
2022-01-29 12:37:55 +0300

    add day mapper

---
+++ lib/data/mapper/day_mapper.dart
@@ -0,0 +1,11 @@
+import 'package:clean_arhitecture_example_app/data/api/model/api_day.dart';
+import 'package:clean_arhitecture_example_app/domain/model/day.dart';
+
+class DayMapper {
+  static Day fromApi(ApiDay day) => Day(
+        sunrise: DateTime.tryParse(day.sunrise) ?? DateTime(0),
+        sunset: DateTime.tryParse(day.sunrise) ?? DateTime(0),
+        solarNoon: DateTime.tryParse(day.sunrise) ?? DateTime(0),
+        dayLength: day.dayLength.toInt(),
+      );
+}
